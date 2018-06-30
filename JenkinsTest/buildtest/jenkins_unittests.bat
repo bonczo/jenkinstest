56822896279bb7756014f59c710c535f0dcabd2e
DESKTOP-MUISLJB\BARTEK <bartipach@gmail.com>
2018-06-30 10:26:37 +0200

    added nuget package and script to run tests

---
+++ JenkinsTest/buildtest/jenkins_unittests.bat
@@ -0,0 +1,24 @@
+REM Path variables
+ SET FolderPath=%1
+ SET ResultsPath=C:\UnitTestResults
+ SET CoverageHistoryPath=C:\CoverageHistory
+ 
+ SET NunitPath=%FolderPath%\packages\NUnit.ConsoleRunner.3.8.0\tools
+ SET ReportUnitPath=%FolderPath%\packages\ReportUnit.1.2.1\tools
+ SET OpenCoverPath=%FolderPath%\packages\OpenCover.4.6.519\tools
+ SET ReportGeneratorPath=%FolderPath%\packages\ReportGenerator.3.1.2\tools
+ 
+ SET UnitTestProj=%FolderPath%\JenkinsTest.Tests\JenkinsTest.Tests.csproj
+ 
+ REM Recreate Results Folder
+ rd /S /Q %ResultsPath%
+ md %ResultsPath%
+ 
+ REM Create coverage history folder if not exists
+ if not exist "%CoverageHistoryPath%" mkdir %CoverageHistoryPath%
+ 
+ REM Run Nunit3 for tests, it produces TestResult.xml report
+ %NunitPath%\nunit3-console.exe %UnitTestProj% --result=%ResultsPath%\TestResult.xml
+ 
+ REM Get nunit command errorlevel
+ SET NunitError=%ERRORLEVEL%
